ef5b3b4201916a22b59d19422360388baad13a0a
YumnaAsim <yumnaasim11@gmail.com>
2017-11-03 18:35:51 +0500

    made form 1

--- app/src/main/java/com/example/yumnaasim/rtirpc/MainActivity.java
+++ app/src/main/java/com/example/yumnaasim/rtirpc/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.yumnaasim.rtirpc;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,95 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.hospital_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.ambulance_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.history_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.travelling_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner3.setAdapter(adapter3);
+
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.vehicles_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner4.setAdapter(adapter4);
+
+        Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
+// Create an ArrayAdapter using the string array and a default spinner layout
+// Apply the adapter to the spinner
+        spinner5.setAdapter(adapter4);
+
+        Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
+                R.array.patient_ride_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner6.setAdapter(adapter5);
+
+        Spinner spinner7 = (Spinner) findViewById(R.id.spinner7);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
+                R.array.helmet_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner7.setAdapter(adapter6);
+
+        Spinner spinner8 = (Spinner) findViewById(R.id.spinner8);
+// Create an ArrayAdapter using the string array and a default spinner layout
+// Apply the adapter to the spinner
+        spinner8.setAdapter(adapter6);
+
+        Spinner spinner9 = (Spinner) findViewById(R.id.spinner9);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(this,
+                R.array.collision_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner9.setAdapter(adapter7);
+
+        Spinner spinner10 = (Spinner) findViewById(R.id.spinner10);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter8 = ArrayAdapter.createFromResource(this,
+                R.array.loc_detail_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner10.setAdapter(adapter8);
     }
 }
