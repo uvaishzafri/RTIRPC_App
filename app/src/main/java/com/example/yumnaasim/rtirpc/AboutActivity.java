e8a362615ef45f6b5fc873dc3e9f61538a42e13e
Yumna Asim <yumnaasim11@gmail.com>
2018-04-04 14:38:48 +0500

    fixed activity name

--- app/src/main/java/com/example/yumnaasim/rtirpc/AboutActivity.java
+++ app/src/main/java/com/example/yumnaasim/rtirpc/AboutActivity.java
@@ -9,7 +9,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        this.setTitle("AboutActivity");
+        this.setTitle("About");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
